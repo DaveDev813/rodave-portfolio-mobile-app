53aeb85905c2788a502efc51d253ecd2f3ab01af
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-12-09 08:38:02 +0800

    offline mode - still has errors

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { WebView } from 'react-native';
+// import { WebView } from 'react-native';
+import OfflineCacheWebView from 'react-native-offline-cache-webview';
 
 export default function App() {
   return (
-    <WebView
+    <OfflineCacheWebView
       useWebKit
       source={{ uri: 'https://rodave-dev.web.app/' }}
       javaScriptEnabled={true}
